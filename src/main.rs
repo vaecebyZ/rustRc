61b34b5182ca52b7a8c5c8afd4232be726c425f4
Sanka <vaeceby2@qq.com>
2022-06-24 10:22:01 +0800

    update | update test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main(){
-    
+fn main() {
+    // 不持有所有权的类型 切片
+    let mut str = String::from("hello world");
+    let res = first_word(&str);
+    str.clear();
+    println!("{}", res)
+}
+
+fn first_word(s: &String) -> usize {
+    let str_byte = s.as_bytes();
+
+    // iter 返回一个可以被枚举的数据集合 enumerate 返回经过包装的元组的
+    for (i, &item) in str_byte.iter().enumerate() {
+        // 模式匹配
+
+        if item == b' ' {
+            return i;
+        }
+
+    }
+
+    s.len()
 }
